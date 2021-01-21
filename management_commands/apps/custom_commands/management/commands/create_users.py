80391e38d5e80641702f6ad6302b5df7d377dd0f
Batyr Jumageldiyew <jumageldiyewb@list.ru>
2021-01-21 16:44:16 +0500

    Add requirements

--- management_commands/apps/custom_commands/management/commands/create_users.py
+++ management_commands/apps/custom_commands/management/commands/create_users.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
+
 class Command(BaseCommand):
     help = 'Create random users'
 
